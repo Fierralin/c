632c802a850b6d3dd739b56650e042135f2dc76d
Fierralin <fierralin@sina.com>
2016-05-23 15:59:53 +0800

    nothing

---
+++ pod.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+#include <cstdlib>
+#include <list>
+
+typedef struct Pod {
+
+} Pod;
+
+typedef struct PodList {
+	int podsize;
+	int podremain;
+	
+	bool *chainrecord;
+	int size_chainrc;
+} PodList;
+
+typedef struct Chain {
+
+} Chain;
+
+typedef struct ChainList {
+	std::list<Chain> chainlist;
+	
+} ChainList;
+
+
+int main(void) {
+	
